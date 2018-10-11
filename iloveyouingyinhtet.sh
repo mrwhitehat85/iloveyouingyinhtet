d99ee42fbf951e36bf4c31aebd0343a152598ace
Hlaing Phyo Aung <43530129+mrwhitehat85@users.noreply.github.com>
2018-10-11 03:46:24 +0000

    Add files via upload

---
+++ iloveyouingyinhtet.sh
@@ -0,0 +1,10 @@
+
+          @@ -256,7 +256,7 @@ figlet Bhai 4 You
+ 256  256  
+ 257  257  
+ 258  258  echo "  "
+ 259      -echo -e $lightgreen "         Script By Sutariya Parixit"
+      259 +echo -e $lightgreen "         Script is Written By Sutariya Parixit"
+ 260  260  echo "   "
+ 261  261  echo -e $lightgreen"  Visit My Site...For More Knowledge about me..!!!"
+ 262  262  
