0f568e13d6d232e1f43fb9cba58ff6f0cb031586
Hlaing Phyo Aung <43530129+mrwhitehat85@users.noreply.github.com>
2018-10-11 03:52:44 +0000

    Add files via upload

---
+++ hlaingphyoaung.sh
@@ -0,0 +1,12 @@
+
+          @@ -29,8 +29,8 @@ echo " "
+  29   29  echo " "
+  30   30  
+  31   31  # install Metasploit-2....
+  32      -mkdir Bulltools
+  33      -cd iloveyouingyinhtet
+       32 +mkdir iloveyouingyinhtet
+       33 +cd iloveyouingyinhtet
+  34   34  apt update
+  35   35  figlet Kingsploit..B4U
+  36   36  git clone https://github.com/mrwhitehat85/iloveyouingyinhtet.git
